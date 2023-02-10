e4907e5a06598c50179978df148bd90bebeb6759
Diego Nogueira <30667583+DiegoSNogueira@users.noreply.github.com>
2023-02-10 10:25:56 -0400

    commit inicial

---
+++ script.js
@@ -0,0 +1,25 @@
+button = document.querySelector('button');
+
+button.addEventListener('click', (event) => {
+    event.preventDefault();
+    let disciplina = document.querySelector('#disciplina').value;
+    let nota1 = parseInt(document.querySelector('#nota1').value);
+    let nota2 = parseInt(document.querySelector('#nota2').value);
+    let nota3 = parseInt(document.querySelector('#nota3').value);
+    let nota4 = parseInt(document.querySelector('#nota4').value);
+
+
+    let media = (nota1 + nota2 + nota3 + nota4) / 4;
+    console.log(media);
+
+    Swal.fire({
+        title: `Resultado - ${disciplina}`,
+        text: `A sua média é ${media.toFixed(2)}`,
+        confirmButtonText: 'Ok'
+    });
+
+
+});
+
+
+
